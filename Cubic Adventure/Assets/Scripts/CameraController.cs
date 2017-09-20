c4757fda6d2e6650b265affe7b28bc90ec9a42bb
wnyao <wenyao_k@hotmail.com>
2017-09-21 03:23:58 +0800

    Level 2 game design & camera control
    Completed Level2 game design and added camera control. Enemy5 still
    unfinished.

---
+++ Cubic Adventure/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;	//target for camera to follow
+	public float smoothing = 5f;
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position; //getting offset
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 targetCamPos = target.position + offset; //set the target camera position
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime); //move the camera using Vector3.Lerp
+	}
+}
---
+++ Cubic Adventure/Assets/Scripts/EnemyRush.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRush : MonoBehaviour {
+
+	public Transform[] rushPoints;	//stores position points that the enemy has to move
+	private int currentPoint;	//current position point of the enemy
+	private float moveSpeed;
+
+	public GameObject enemy;
+	private bool checker;
+
+	// Use this for initialization
+	void Start () {
+		currentPoint = 0;
+		checker = false;
+	}
+		
+	void OnTriggerEnter(Collider other) {
+
+		if (other.transform.tag == "Player") {
+			checker = true;
+		}
+	}
+
+	void Update() {
+		if (checker == true) {
+			currentPoint += 1;
+			moveSpeed = 6;
+
+			if (currentPoint < rushPoints.Length) {
+				currentPoint = 0;
+				moveSpeed = 2;
+			}
+
+			enemy.transform.position = Vector3.MoveTowards (transform.position, rushPoints[currentPoint].position, moveSpeed * Time.deltaTime);
+		}
+	}
+
+}
