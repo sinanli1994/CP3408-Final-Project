91ee1015bf19444696963abb8200fa5864e2d9d5
sinanli1994 <sinan.li@my.jcu.edu.au>
2017-09-25 21:27:30 +0800

    Fix the bug of GameManager script

--- Cubic Adventure/Assets/Scripts/GameManager.cs
+++ Cubic Adventure/Assets/Scripts/GameManager.cs
@@ -4,24 +4,12 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-	public int currentScore;
-	public int highScore;
-
 	public int currentLevel;
-	public int unlockedLevel;
-
 	public GUISkin skin;
-	//public float startTime;
-	//public float currentTime;
 
 	void Start()
 	{
 		DontDestroyOnLoad (gameObject);
-	}
-
-	void Update() { //happen every single frame
-		//startTime -= Time.deltaTime;
-
 	}
 
 	public void CompleteLevel() // Handle the loading of next level
@@ -30,10 +18,8 @@ public class GameManager : MonoBehaviour {
 
 		if (currentLevel >= 0) {
 			currentLevel += 1; //increment to next scene
-			if (currentLevel < 4) {
+			if (currentLevel < 5) {
 				SceneManager.LoadScene (currentLevel);
-			} else {
-				print ("You Win");
 			}
 		} 
 	}
